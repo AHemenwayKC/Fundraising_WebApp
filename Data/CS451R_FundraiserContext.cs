b0fd9c6f0e31957f438d9156ce33f959f595101f
KyleHelm <124329289+KyleHelm@users.noreply.github.com>
2023-02-15 21:11:36 -0600

    Add files via upload

---
+++ Data/CS451R_FundraiserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CS451R_Fundraiser.Models;
+
+namespace CS451R_Fundraiser.Data
+{
+    public class CS451R_FundraiserContext : DbContext
+    {
+        public CS451R_FundraiserContext (DbContextOptions<CS451R_FundraiserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CS451R_Fundraiser.Models.Fundraiser> Fundraiser { get; set; } = default!;
+    }
+}
