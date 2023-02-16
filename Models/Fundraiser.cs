f47ef55cdedc77170a466aa9087f0a797e786927
KyleHelm <124329289+KyleHelm@users.noreply.github.com>
2023-02-15 21:15:58 -0600

    Add files via upload

---
+++ Models/Fundraiser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS451R_Fundraiser.Models
+{
+    public class Fundraiser
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [Display(Name = "Post Date")]
+        [DataType(DataType.Date)]
+        public DateTime PostDate { get; set; }
+        public string? Category { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Goal { get; set; }
+    }
+}
---
+++ Models/FundraiserCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace CS451R_Fundraiser.Models;
+
+public class FundraiserCategoryViewModel
+{
+    public List<Fundraiser>? Fundraisers { get; set; }
+    public SelectList? Categories { get; set; }
+    public string? FundraiserCategory { get; set; }
+    public string? SearchString { get; set; }
+}
