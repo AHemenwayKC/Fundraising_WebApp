0fe87c45058b23c9449b8225fa21040237b5c0dd
KyleHelm <124329289+KyleHelm@users.noreply.github.com>
2023-02-15 21:20:04 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 using CS451R_Fundraiser.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CS451R_Fundraiser.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CS451R_FundraiserContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CS451R_FundraiserContext") ?? throw new InvalidOperationException("Connection string 'CS451R_FundraiserContext' not found.")));
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -15,6 +19,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
