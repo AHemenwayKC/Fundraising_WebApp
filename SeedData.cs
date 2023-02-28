d4ce0dddec2bf5664ed5f990a46cf0c0384563cc
rbro3551 <rileybrookins2@gmail.com>
2023-02-27 20:49:11 -0600

    fixed duplicate data

--- SeedData.cs
+++ SeedData.cs
@@ -12,8 +12,14 @@ namespace CS451R_Fundraiser
             using var scope = host.Services.CreateScope();
             using var context = scope.ServiceProvider.GetRequiredService<CS451R_FundraiserContext>();
             context.Database.EnsureCreated();
+            if (context.Fundraiser.Any())
+            {
+                return;   // DB has been seeded
+            }
             AddFundraisers(context);
         }
+
+
 
         private static void AddFundraisers(CS451R_FundraiserContext context)
         {
@@ -22,6 +28,7 @@ namespace CS451R_Fundraiser
 
             context.Fundraiser.Add(new Fundraiser
             {
+                Id = 1234,
                 Title = "Help little Timmy get new legs",
                 PostDate = DateTime.Parse("2023-2-12"),
                 Category = "Prosthetics",
@@ -30,6 +37,7 @@ namespace CS451R_Fundraiser
 
             context.Fundraiser.Add(new Fundraiser
             {
+                Id = 1235,
                 Title = "Help me buy a new BMW",
                 PostDate = DateTime.Parse("2020-3-13"),
                 Category = "Transportation",
@@ -38,6 +46,7 @@ namespace CS451R_Fundraiser
 
             context.Fundraiser.Add(new Fundraiser
             {
+                Id = 1236,
                 Title = "My mom needs help paying for college",
                 PostDate = DateTime.Parse("2021-5-23"),
                 Category = "Education",
@@ -48,60 +57,3 @@ namespace CS451R_Fundraiser
         }
     }
 }
-
-
-
-
-
-//    public static class SeedData
-//    {
-//        public static void Initialize(IServiceProvider serviceProvider)
-//        {
-//            using (var context = new CS451R_FundraiserContext(
-//                serviceProvider.GetRequiredService<
-//                    DbContextOptions<CS451R_FundraiserContext>>()))
-//            {
-//                // Look for any movies.
-//                if (context.Fundraiser.Any())
-//                {
-//                    return;   // DB has been seeded
-//                }
-//                context.Fundraiser.AddRange(
-//                    new Fundraiser
-//                    {
-//                        Id = 1234,
-//                        Title = "Help little Timmy get new legs",
-//                        PostDate = DateTime.Parse("2023-2-12"),
-//                        Category = "Prosthetics",
-//                        Goal = 100000
-//                    },
-//                    new Fundraiser
-//                    {
-//                        Id = 1235,
-//                        Title = "Help me buy a new BMW",
-//                        PostDate = DateTime.Parse("2020-3-13"),
-//                        Category = "Transportation",
-//                        Goal = 85000
-//                    },
-//                    new Fundraiser
-//                    {
-//                        Id = 1236,
-//                        Title = "My mom needs help paying for college",
-//                        PostDate = DateTime.Parse("2021-5-23"),
-//                        Category = "Education",
-//                        Goal = 25000
-//                    },
-//                    new Fundraiser
-//                    {
-//                        Id = 1237,
-//                        Title = "Support cancer patients",
-//                        PostDate = DateTime.Parse("2019-4-15"),
-//                        Category = "Medical",
-//                        Goal = 250000
-//                    }
-//                );
-//                context.SaveChanges();
-//            }
-//        }
-//    }
-//}
