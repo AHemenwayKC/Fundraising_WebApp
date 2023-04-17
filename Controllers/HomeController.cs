ef9bd2e5f7fe0c81068e3267a606b5dcb2025ce0
KyleHelm <kehrh2@umsystem.edu>
2023-04-17 14:04:03 -0500

    Added 4 basic unit tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,9 @@ namespace CS451R_Fundraiser.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Search()
-        {
-            return View();
-        }
-
-        public IActionResult Dashboard()
-        {
-            return View();
+            return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
